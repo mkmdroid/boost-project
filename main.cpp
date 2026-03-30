c4a5609264cacc46ca55e7819b5a2e89d62a0ae6
mkmdriod <mohamedkhaledmkmauc@aucegypt.edu>
2026-03-30 23:32:44 +0200

    Add main.cpp and CMakeLists.txt

---
+++ main.cpp
@@ -0,0 +1,41 @@
+#define BOOST_ASIO_HAS_IO_URING 1
+#define BUFFER_SIZE 10
+
+#include <boost/asio.hpp>
+#include <iostream>
+#include <vector>
+
+int main() {
+  boost::asio::io_context io_context;
+
+  std::vector<char> arr(BUFFER_SIZE);
+  std::vector<char> results;
+
+  boost::asio::stream_file stream1(io_context, "1.txt", boost::asio::stream_file::read_only);
+  stream1.async_read_some(
+      boost::asio::buffer(arr),
+      [&](const boost::system::error_code& ec, std::size_t n) {
+        if (!ec) {
+          std::cout << "Read " << n << " bytes from stream" << std::endl;
+          results.insert(results.end(), arr.begin(), arr.begin() + n);
+        }
+      });
+
+  boost::asio::stream_file stream2(io_context, "2.txt", boost::asio::stream_file::read_only);
+  stream2.async_read_some(
+      boost::asio::buffer(arr),
+      [&](const boost::system::error_code& ec, std::size_t n) {
+        if (!ec) {
+          std::cout << "Read " << n << " bytes from stream" << std::endl;
+          results.insert(results.end(), arr.begin(), arr.begin() + n);
+        }
+      });
+
+  io_context.run();
+
+  for (auto c : results) {
+    std::cout << c;
+  }
+
+  return 0;
+}
