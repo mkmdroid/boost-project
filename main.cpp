0d7cd4c01b317bd616c6ef34bf31ba2217bf0042
mkmdriod <mohamedkhaledmkmauc@aucegypt.edu>
2026-04-18 13:47:46 +0200

    Using two separated buffers for correct output 1 and 2

--- main.cpp
+++ main.cpp
@@ -8,26 +8,27 @@
 int main() {
   boost::asio::io_context io_context;
 
-  std::vector<char> arr(BUFFER_SIZE);
+  std::vector<char> arr1(BUFFER_SIZE);
+  std::vector<char> arr2(BUFFER_SIZE);
   std::vector<char> results;
 
   boost::asio::stream_file stream1(io_context, "1.txt", boost::asio::stream_file::read_only);
   stream1.async_read_some(
-      boost::asio::buffer(arr),
+      boost::asio::buffer(arr1),
       [&](const boost::system::error_code& ec, std::size_t n) {
         if (!ec) {
           std::cout << "Read " << n << " bytes from stream" << std::endl;
-          results.insert(results.end(), arr.begin(), arr.begin() + n);
+          results.insert(results.end(), arr1.begin(), arr1.begin() + n);
         }
       });
 
   boost::asio::stream_file stream2(io_context, "2.txt", boost::asio::stream_file::read_only);
   stream2.async_read_some(
-      boost::asio::buffer(arr),
+      boost::asio::buffer(arr2),
       [&](const boost::system::error_code& ec, std::size_t n) {
         if (!ec) {
           std::cout << "Read " << n << " bytes from stream" << std::endl;
-          results.insert(results.end(), arr.begin(), arr.begin() + n);
+          results.insert(results.end(), arr2.begin(), arr2.begin() + n);
         }
       });
 
